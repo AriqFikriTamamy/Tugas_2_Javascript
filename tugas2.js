ffb70f992f828aa5c952b66209817f4f64061d1e
Ariq Fikri Tamamy <ariqrick@gmail.com>
2021-07-07 17:53:58 +0700

    Tugas 1 Javascript

---
+++ tugas2.js
@@ -0,0 +1,19 @@
+var umur = 20;
+var jeniskelamin = "laki-laki";
+var gemuk = false;
+var kg = 68;
+var pacar = "tidak ada"
+pacar = null;
+var tinggi = ["140cm","150cm","160cm","170cm","180cm"];
+var masadepan;
+
+if(kg>60)
+gemuk = true;
+else
+gemuk = false;
+console.log(umur);
+console.log(jeniskelamin);
+console.log(gemuk);
+console.log(pacar);
+console.log(tinggi[4])
+console.log(masadepan)
